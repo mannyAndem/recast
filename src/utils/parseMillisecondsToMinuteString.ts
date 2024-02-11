23a55506ed7377d95fcc85d22f5e4671929c283e
Emmanuel Andem <emmanuelandem360@gmail.com>
2024-02-11 13:54:44 +0100

    fix: apd zeros t0 resultant time string to always ensure they're 2 digits

--- src/utils/parseMillisecondsToMinuteString.ts
+++ src/utils/parseMillisecondsToMinuteString.ts
@@ -1,4 +1,10 @@
 export const parseMillisecondsToMinuteString = (milliseconds: number) => {
   let seconds = milliseconds / 1000;
-  return `${Math.floor(seconds / 60)}:${Math.floor(seconds % 60)}`;
+  let minuteString = String(Math.floor(seconds / 60));
+  let secondString = String(Math.floor(seconds % 60));
+
+  secondString = secondString.padStart(2, "0");
+  minuteString = minuteString.padStart(2, "0");
+
+  return `${minuteString}:${secondString}`;
 };
